95220c10d14e6ece019797a98f1013518d98fbd4
Awni Hannun <awni@apple.com>
2024-05-04 19:29:14 -0700

    allow exception to be caught in main thread

--- mlx/ops.cpp
+++ mlx/ops.cpp
@@ -2633,6 +2633,13 @@ array scatter(
     idx = astype(idx, dtype, s);
   }
 
+  // TODO, remove when scatter supports 64-bit outputs
+  if (size_of(a.dtype()) == 8) {
+    std::ostringstream msg;
+    msg << "[scatter] Does not support " << a.dtype();
+    throw std::invalid_argument(msg.str());
+  }
+
   inputs.insert(inputs.begin(), a);
   // TODO promote or cast?
   inputs.push_back(astype(updates, a.dtype(), s));
